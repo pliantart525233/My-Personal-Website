f0e5ec557954db625dd6732953c238db7d197e9c
pliantart525233 <67117443+pliantart525233@users.noreply.github.com>
2023-07-24 15:38:51 -0300

    Upgrade

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+/*
 // Initial state
 var scrollPos = 0;
 // adding scroll event
@@ -14,4 +14,46 @@ window.addEventListener('scroll', function(){
     }
 	// saves the new position for iteration.
 	scrollPos = (document.body.getBoundingClientRect()).top;
-});
+});*/
+
+/*window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("FixedNavBar").style.fontSize = "20px";
+  } else {
+    document.getElementById("FixedNavBar").style.fontSize = "40px";
+  }
+}*/
+
+window.onscroll = function() 
+{
+  
+  scrollFunction()
+
+};
+
+function scrollFunction()
+
+{
+
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) 
+  {
+
+    document.getElementById("FixedNavBar").style.backgroundColor = "#353535";
+    document.getElementById("Title").style.color = "#D8D8D8";
+    document.getElementsByClassName("AboutMeButton").style.backgroundColor = "#353535";
+
+  }
+
+  else
+  {
+
+    document.getElementById("FixedNavBar").style.backgroundColor = "#D8D8D8";
+    document.getElementById("Title").style.color = "#353535";
+    document.getElementsByClassName("AboutMeButton").style.backgroundColor = "#D8D8D8";
+    document.getElementsByClassName("AboutMeButton").style.color = "#D8D8D8";
+
+  }
+
+}
